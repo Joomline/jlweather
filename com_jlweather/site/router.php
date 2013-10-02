f68d5be5891566b2c1ec057a52216c518b419e63
zikkuratvk <vadim@joomline.ru>
2013-10-02 11:41:24 +0900

    фикс роутера смена версии

--- com_jlweather/site/router.php
+++ com_jlweather/site/router.php
@@ -1,12 +1,17 @@
 <?php
  /**
- * @package com_afisha
- * @author Artem Zhukov (artem@joomline.ru), Anton Voynov (anton@joomline.net)
- * @version 4.6
- * @copyright (C) 2009-2012 by JoomLine (http://www.joomline.net)
- * @license JoomLine: http://joomline.net/licenzija-joomline.html
+ * JLweather - components of the weather for joomla.
  *
-*/
+ * @version 2.6.1
+ * @package JL Weather
+ * @author Artem Zhukov (artem@joomline.ru) Anton Voynov (anton@joomline.ru)
+ * @copyright (C) 2013 by Anton Voynov(http://www.joomline.ru)
+ * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
+ *
+ * If you fork this to create your own project,
+ * please make a reference to JoomLine someplace in your code
+ * and provide a link to http://www.joomline.ru
+ **/ 
     function JlweatherBuildRoute(&$query) {
        $segments = array();
         if(isset( $query['cid'] )) {
@@ -36,7 +41,7 @@ function JlweatherParseRoute($segments) {
 		if (count($city_list)>0) {
 			foreach ($city_list as $k =>$v) {
 				$city_list[$k] = explode('#',$v);
-				if ($city_list[$k][1]==$segments[0]){$c_id = $city_list[$k][0];}
+				if ($city_list[$k][1]==str_replace(':','-',$segments[0])){$c_id = $city_list[$k][0];}
 			}
 		}
 	
