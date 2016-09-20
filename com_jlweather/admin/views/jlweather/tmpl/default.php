c5beeae4ed9116611e4d0205b2192f9eb91618ab
zikkuratvk <zikkuratvk@gmail.com>
2016-09-20 20:53:44 +0200

    завершено выделение языковых переменных Новые виджеты доната.

--- com_jlweather/admin/views/jlweather/tmpl/default.php
+++ com_jlweather/admin/views/jlweather/tmpl/default.php
@@ -1,77 +1,16 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 
-<p><?=JText::_('DESC_ADMIN') ?></p>
-<p><?=JText::_('DESC_WEATER') ?></p>
-<p><?=JText::_('DESC_DEVELOPERS') ?></p>
-<p><?=JText::_('COM_JLWEATHER_DESC_SETTINGS') ?></p>
+<p><?php echo JText::_('DESC_ADMIN') ?></p>
+<p><?php echo JText::_('DESC_WEATER') ?></p>
+<p><?php echo JText::_('DESC_DEVELOPERS') ?></p>
+<p><?php echo JText::_('COM_JLWEATHER_DESC_SETTINGS') ?></p>
 <p></p>
-    <h1>Donate (Пожертвования)</h1>
+    <h1><?php echo JText::_('COM_JL_WEATHER_DONATE_TITLE') ?></h1>
 <div class="donate">
-    <h2>Для России и Украины</h2>
-    <table class="adminlist">
-        <thead>
-        <tr>
-            <th width="33%">Webmoney</th>
-            <th width="33%">Яндекс деньги</th>
-            <th>PayPal</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td align="center">
-                R187492847899<br/>
-                Z346259799702
-            </td>
-            <td align="center">
-                41001531672137
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="WKKRA6YE9K6DU">
-                    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-                    <img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
-                </form>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <h2>For all the world</h2>
-    <p>If you use our free products, you can support us and help create and further develop free extensions, making a donation.</p>
-    <h3>PayPal</h3>
-    <table class="adminlist">
-        <thead>
-        <tr>
-            <th>
-                Donate 5$
-            </th>
-            <th>
-                Donate 10$
-            </th>
-            <th>
-                Donate 20$
-            </th>
-            <th>
-                Donate any amount
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="EUPK7JC2X9D9Q" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="4PKE57MZSXA9S" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="CFTSDVVQP4QL6" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="WKKRA6YE9K6DU" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal — The safer, easier way to pay online." /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <p>We ask that you specify in the payment, contact for feedback.</p>
+    <p>
+        <?php echo JText::_('COM_JL_WEATHER_DONATE_TEXT') ?>
+    </p>
+    <div>
+        <?php echo JText::_('COM_JL_WEATHER_DONATE') ?>
+    </div>
 </div>
